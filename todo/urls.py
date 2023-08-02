6cefb6b6c969067e6d9027649a3d8a37a5a05904
ArsenijVideman <mysenyaswork@gmail.com>
2023-08-02 23:23:34 +0300

    1.1

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,6 @@ from .views import Feature
 urlpatterns = [
     path('', Feature.index, name='home'),
     path('about/', Feature.about_me, name='about'),
-    path('create/', Feature.create_new_task, name='create')
+    path('create/', Feature.create_new_task, name='create'),
+    path('resume/', Feature.resume, name='resume'),
 ]
